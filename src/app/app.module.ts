9a09269a3167151897df8c56ae2a79cad20475b0
barbosathiago <thiago.barbosa@infouniron.com>
2017-11-25 13:56:20 -0400

    Criação do Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
     AppComponent,
     HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
