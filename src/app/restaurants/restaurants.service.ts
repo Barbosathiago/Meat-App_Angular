71053707f8ab07537ae75b5f7227976d75b5330a
barbosathiago <thiago.barbosa@infouniron.com>
2017-11-25 18:32:31 -0400

    Implementação de um error handler genérico
    Implementado um errorhandler através de um método estático utilizado na função catch do observable

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,10 +3,12 @@ import {Http} from '@angular/http'
 
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import {Restaurant} from "./restaurant/restaurant.model"
 
 import {MEAT_API} from '../app.api'
+import {ErrorHandler} from '../app.error-handler'
 
 
 @Injectable()
@@ -15,7 +17,9 @@ export class RestaurantsService {
   constructor(private http: Http){}
 
   restaurants():Observable<Restaurant[]>{
-    return this.http.get(`${MEAT_API}/restaurants`)
+    return this.http.get(`${MEAT_API}/restaurants1`)
       .map(response => response.json())
-  }
+      .catch(ErrorHandler.handleError)
+    }
+
 }
