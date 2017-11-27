b9ed2bd1bd7964c8851aee387395568e13b20619
barbosathiago <thiago.barbosa@infouniron.com>
2017-11-26 21:39:01 -0400

    Configurado o componente Review para receber e passar os dados das reviews através do serviço

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../../restaurants/restaurants.service'
+import {ActivatedRoute} from '@angular/router'
+
+
+import {Observable} from 'rxjs/Observable'
 
 @Component({
   selector: 'mt-reviews',
-  templateUrl: './reviews.component.html',  
+  templateUrl: './reviews.component.html',
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>
+
+  constructor(private reestaurantsService: RestaurantsService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.reestaurantsService
+      .reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
