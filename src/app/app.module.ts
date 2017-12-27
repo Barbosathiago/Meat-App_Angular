62fb23d60e60cdf64e45606fad3b707fe3f30f14
barbosathiago <thiago.barbosa@infouniron.com>
2017-12-27 00:13:26 -0400

    Criado um snackbar com todas as dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule} from './shared/shared.module'
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -38,6 +38,7 @@ import { SharedModule} from './shared/shared.module'
   imports: [
     BrowserModule,
     HttpModule,
+    BrowserAnimationsModule,
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
